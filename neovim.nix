d9f8e244f7fd1b9193698f2ede6d140925cfcb51
Matteo Bongiovanni <MatteoBongiovanni@proton.me>
2024-06-13 20:11:08 +0200

    init

---
+++ neovim.nix
@@ -0,0 +1,21 @@
+{ config, lib, pkgs, ... }:
+{
+  options = {
+    neovim.enable = lib.mkEnableOption "enable neovim packages";
+  };
+  config = lib.mkIf config.neovim.enable {
+    environment.sessionVariables = {
+      EDITOR = "nvim";
+    };
+    environment.systemPackages = with pkgs; [
+      neovim
+      ripgrep
+      wget
+      git
+      gcc
+      unzip
+      wl-clipboard #wayland
+      xxd
+    ];
+  };
+}

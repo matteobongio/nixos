a7825fd38303cb921185d949648c421a42cc3d9a
Matteo Bongiovanni <MatteoBongiovanni@proton.me>
2024-10-12 15:31:08 +0200

    docker

---
+++ programming/docker.nix
@@ -0,0 +1,16 @@
+{ config, lib, pkgs, ... }:
+{
+  options = {
+    docker.enable = lib.mkEnableOption "enable docker";
+  };
+  config = lib.mkIf config.docker.enable {
+    virtualisation.docker.enable = true;
+    environment.systemPackages = with pkgs; [
+      docker
+    ];
+    # virtualisation.docker.rootless = {
+    #   enable = true;
+    #   setSocketVariable = true;
+    # };
+  };
+}

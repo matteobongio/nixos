7d9a07188c5c5679041a0791135433200a66cdac
Matteo Bongiovanni <MatteoBongiovanni@proton.me>
2024-10-14 21:27:42 +0200

    add NVIDIA VULKAN support after almost a year

--- configuration.nix
+++ configuration.nix
@@ -53,8 +53,8 @@
 
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
+  services.xserver.enable = false;
+  services.displayManager.sddm.wayland.enable = true;
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;

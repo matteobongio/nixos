3571728aabafa201af7fb6fe10ae8ae7ff0de84a
Matteo Bongiovanni <MatteoBongiovanni@proton.me>
2024-10-17 16:14:41 +0200

    tlp

--- hosts/aster-nixos/hardware-configuration.nix
+++ hosts/aster-nixos/hardware-configuration.nix
@@ -52,15 +52,24 @@
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+      PLATFORM_PROFILE_ON_BAT="low-power";
 
       CPU_MIN_PERF_ON_AC = 0;
       CPU_MAX_PERF_ON_AC = 100;
+      
       CPU_MIN_PERF_ON_BAT = 0;
       CPU_MAX_PERF_ON_BAT = 40;
+
+      # Turbo Boost
+      CPU_BOOST_ON_AC=1;
       CPU_BOOST_ON_BAT=0;
+
+      CPU_HWP_DYN_BOOST_ON_AC=1;
       CPU_HWP_DYN_BOOST_ON_BAT=0;
+
       #Optional helps save long term battery health
       START_CHARGE_THRESH_BAT0 = 50; # 50 and bellow it starts to charge
       STOP_CHARGE_THRESH_BAT0 = 90; # 90 and above it stops charging

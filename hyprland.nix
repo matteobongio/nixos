6deb59ab411cda63f87ed7ec2b0117bdff68fe49
Matteo Bongiovanni <MatteoBongiovanni@proton.me>
2024-10-20 09:49:37 +0200

    rose pine moon

--- hyprland.nix
+++ hyprland.nix
@@ -30,7 +30,7 @@
       url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
       sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
     };
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine-moon.yaml";
     polarity = "dark";
     targets.gnome.enable = true;
     targets.gtk.enable = true;

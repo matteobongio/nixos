486ec5a0a9700b3d878a1734f7a4b96c50074c24
Matteo Bongiovanni <MatteoBongiovanni@proton.me>
2024-10-21 17:40:51 +0200

    remove stylix

--- flake.nix
+++ flake.nix
@@ -4,11 +4,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    stylix.url = "github:danth/stylix";
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+    # stylix.url = "github:danth/stylix";
   };
 
   outputs = { self, nixpkgs, nixos-hardware, home-manager, stylix, ... }@inputs: {
@@ -20,18 +20,18 @@
         ./hosts/aster-nixos/hardware-configuration.nix
         nixos-hardware.nixosModules.system76-gaze18
         nixos-hardware.nixosModules.common-cpu-intel
-        stylix.nixosModules.stylix
-        home-manager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.backupFileExtension = "backup";
-          home-manager.useUserPackages = true;
-          home-manager.sharedModules = [{
-            stylix.autoEnable = false;
-            stylix.targets.kde.enable = true;
-            stylix.targets.firefox.enable = true;
-          }];
-          home-manager.users.matteob = import ./home.nix;
-        }
+        # stylix.nixosModules.stylix
+        # home-manager.nixosModules.home-manager {
+        #   home-manager.useGlobalPkgs = true;
+        #   home-manager.backupFileExtension = "backup";
+        #   home-manager.useUserPackages = true;
+        #   home-manager.sharedModules = [{
+        #     stylix.autoEnable = false;
+        #     stylix.targets.kde.enable = true;
+        #     stylix.targets.firefox.enable = true;
+        #   }];
+        #   home-manager.users.matteob = import ./home.nix;
+        # }
       ];
     };
   };
--- hyprland.nix
+++ hyprland.nix
@@ -16,27 +16,23 @@
     brightnessctl
     xdg-desktop-portal-hyprland
     kdePackages.xdg-desktop-portal-kde
-    dolphin
-    kdePackages.kio-fuse
-    kdePackages.kio-extras
-    base16-schemes
   ];
   
 
-  stylix = {
-    enable = true;
-    autoEnable = false;
-    image = pkgs.fetchurl {
-      url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
-      sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
-    };
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine-moon.yaml";
-    polarity = "dark";
-    targets.gnome.enable = true;
-    targets.gtk.enable = true;
-    homeManagerIntegration.autoImport = true;
-    homeManagerIntegration.followSystem = true;
-  };
+  # stylix = {
+  #   enable = true;
+  #   autoEnable = false;
+  #   image = pkgs.fetchurl {
+  #     url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
+  #     sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
+  #   };
+  #   base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine-moon.yaml";
+  #   polarity = "dark";
+  #   targets.gnome.enable = true;
+  #   targets.gtk.enable = true;
+  #   homeManagerIntegration.autoImport = true;
+  #   homeManagerIntegration.followSystem = true;
+  # };
 
 
   services.gnome.gnome-keyring.enable = true;

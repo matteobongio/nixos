0e1a1e2af6e253c7e1ba904ef71f112d27172858
Matteo Bongiovanni <MatteoBongiovanni@proton.me>
2024-10-28 12:58:49 +0100

    cleanup

--- hyprland.nix
+++ hyprland.nix
@@ -18,24 +18,7 @@
     kdePackages.xdg-desktop-portal-kde
     networkmanagerapplet
   ];
-  
-
-  # stylix = {
-  #   enable = true;
-  #   autoEnable = false;
-  #   image = pkgs.fetchurl {
-  #     url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
-  #     sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
-  #   };
-  #   base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine-moon.yaml";
-  #   polarity = "dark";
-  #   targets.gnome.enable = true;
-  #   targets.gtk.enable = true;
-  #   homeManagerIntegration.autoImport = true;
-  #   homeManagerIntegration.followSystem = true;
-  # };
-
-
+  security.polkit.enable = true;
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.hyprland.enableGnomeKeyring = true;
   programs.hyprland = {

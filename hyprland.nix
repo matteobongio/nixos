dad7c435ecd7f34a55fb769ef36f0dc794f65959
Matteo Bongiovanni <MatteoBongiovanni@proton.me>
2024-11-11 15:18:45 +0100

    polkit

--- hyprland.nix
+++ hyprland.nix
@@ -17,8 +17,25 @@
     xdg-desktop-portal-hyprland
     xdg-desktop-portal-kde
     networkmanagerapplet
+    polkit_gnome
   ];
   security.polkit.enable = true;
+# environment.pathsToLink = [ "${pkgs.kdePackages.polkit-kde-agent-1}/libexec" ];
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.hyprland.enableGnomeKeyring = true;
   programs.hyprland = {

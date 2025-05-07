fb09bbf25f798bf4afea3a9fdc46344d045bc54a
Matteo Bongiovanni <MatteoBongiovanni@proton.me>
2025-05-07 10:56:58 +0200

    virt

---
+++ programming/virt.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, ... }:
+{
+  options = {
+    virt.enable = lib.mkEnableOption "enable VM packages";
+  };
+  config = lib.mkIf config.virt.enable {
+    programs.virt-manager.enable = true;
+    users.groups.libvirtd.members = [ "matteob" ];
+    virtualisation.libvirtd.enable = true;
+    virtualisation.spiceUSBRedirection.enable = true;
+    # virtualisation.vmware.host.enable = false;
+    # virtualisation.waydroid.enable = false;
+  };
+}
